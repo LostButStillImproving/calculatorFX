8e6991c7176193a25972d320646739c7c1fda457
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-11-27 22:07:09 +0100

    minor

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Kinda Scientific Calculator");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
